3b8a1ebe7850156c50772d01227b47bbb014cb3a
Vitalii36 <vitaliipravdych@gmail.com>
2020-10-21 10:25:45 +0300

    2day

--- Ex_9_Weather_Observation_Station_5.sql
+++ Ex_9_Weather_Observation_Station_5.sql
@@ -1,8 +1,8 @@
-// Query the two cities in STATION with the shortest and longest
+/* Query the two cities in STATION with the shortest and longest
 CITY names, as well as their respective lengths (i.e.: number
 of characters in the name). If there is more than one smallest
 or largest city, choose the one that comes first when ordered
- alphabetically.//
+ alphabetically.*/
 
 SELECT * FROM (SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1) AS T1
 UNION ALL
